aff63bcf3f642b68eb24415ef7934899db414614
CristianATZ <cristiantorreszvl@gmail.com>
2025-06-12 23:04:01 -0600

    refactor: tema de la aplicacion

--- app/src/main/java/com/devtorres/catapi/MainActivity.kt
+++ app/src/main/java/com/devtorres/catapi/MainActivity.kt
@@ -6,14 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.devtorres.catapi.navigation.CatApiNavhost
 import com.devtorres.designsystem.theme.CatApiTheme
 import dagger.hilt.android.AndroidEntryPoint
